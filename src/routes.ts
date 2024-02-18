5ab7e5484945a01408fa8130cf3766a40fc80716
Jeff Toppings <jeffrey.toppings@outlook.com>
2024-02-18 14:52:28 -0600

    feat: setup some basic sample routes

--- src/routes.ts
+++ src/routes.ts
@@ -6,4 +6,13 @@ router.get('/', (req, res) => {
   res.send('Hello from the main route of my TypeScript NodeJS backend!');
 });
 
+router.get('/mexican-train', (req, res) => {
+  res.send('Hello from the main route of mexican-train!');
+});
+
+// Catch-all route for 404 errors within this specific routes file
+router.use((req, res) => {
+  res.status(404).send('404 - Not Found within routes.ts');
+});
+
 export default router;
